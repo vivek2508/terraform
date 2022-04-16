cbdefdb7be111f409533e4e0afadd7e4e3a0abe2
vivek2508 <103503097+vivek2508@users.noreply.github.com>
2022-04-16 18:34:27 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "MyRg"
+    storage_account_name = "storage1509"
+    container_name       = "conatiner1509"
+    key                  = "terraformgithubexample.tfstate"
+  }
+}
+ 
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
+}
+ 
+data "azurerm_client_config" "current" {}
+ 
+#Create Resource Group
+resource "azurerm_resource_group" "tamops" {
+  name     = "tamops"
+  location = "eastus2"
+}
