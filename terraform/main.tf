962ab56e3d0d95192bbed78b5d199dbccae8340f
vivek2508 <103503097+vivek2508@users.noreply.github.com>
2022-04-16 20:24:19 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,16 +5,7 @@ terraform {
     container_name       = "conatiner1509"
     key                  = "terraformgithubexample.tfstate"
   }
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.2.0"
-    }
-  }
-
-provider "azurerm" {
-  # Configuration options
-}
+ 
 }
  
 data "azurerm_client_config" "current" {}
