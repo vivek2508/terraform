aa404a666c4b92fd02c476aec6c4b6649021efc6
vivek2508 <103503097+vivek2508@users.noreply.github.com>
2022-04-17 13:37:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,12 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+  
+terraform {
   backend "azurerm" {
     resource_group_name  = "MyRg"
     storage_account_name = "storage1509"
@@ -6,21 +14,15 @@ terraform {
     key                  = "terraformgithubexample.tfstate"
   }
 }
-required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  
-  required_version = ">= 1.1.0"
-}
-
 provider "azurerm" {
   features {}
+  # subscription_id = var.subscription_id
+  # client_id       = var.client_id
+  # client_secret   = var.client_secret
+  # tenant_id       = var.tenant_id
 }
-
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
-  location = "westus2"
+  location = "eastus"
 }
   
